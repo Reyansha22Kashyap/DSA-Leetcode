fcbb6b1c61b38a83d5152a781089c85a8335a159
Reyansha Kashyap <221075540+Reyansha22Kashyap@users.noreply.github.com>
2026-07-25 14:03:44 +0530

    Fixed variable name and logic in sortedSquares function
    Refactor the sortedSquares function to use the correct variable name and improve logic for handling negative and positive numbers.

--- #977 Squares of a sorted Array.cpp
+++ #977 Squares of a sorted Array.cpp
@@ -1,43 +1,54 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int siz=a.size();
+        int siz=nums.size();
         vector<int> neg;
         vector<int> pos;
 
         for(int i=0;i<siz;i++){
-            if(a[i]<0){
-                neg.push_back(a[i]);
+            if(nums[i]<0){
+                neg.push_back(nums[i]);
             }else{
-                pos.push_back(a[i]);
+                pos.push_back(nums[i]);
             }
-        }-
-        if(neg.size==0){
+        }
             for(int i=0;i<pos.size();i++){
                 pos[i]=pos[i]*pos[i];
-                return pos;
 
             }
-        }
-        if(pos.size==0){
+        
+        
             for(int i=0;i<neg.size();i++){
                 neg[i]=neg[i]*neg[i];
-                reverse(neg.begin(),neg.end());
-                return neg;
-            }
-        }
+            } 
+             reverse(neg.begin(),neg.end());
+         
         int i=0,j=0;
         int id=0;
         int n=neg.size();
         int m=pos.size();
         vector<int> res(n+m);
-        for(int i=0;i<n;i++){
-            neg[i]=neg[i]*neg[i];
-            reverse(neg.begin(),neg.end());
+        while(i<n && j<m){
+           if(neg[i]<pos[j]){
+            res[id]=neg[i];
+            i++;
+            id++;
+           }else{
+            res[id]=pos[j];
+            j++;
+            id++;
+           }
         }
-        for(int i=0;i<n;i++){
-            pos[i]=pos[i]*pos[i];
-    } while(i<n and j<m){
-
+        while(i<n){        
+            res[id]=neg[i];
+            i++;
+            id++;
+        }
+        while(j<m){
+            res[id]=pos[j];
+            j++;
+            id++;
+        } 
+           return res;
     }
 };
